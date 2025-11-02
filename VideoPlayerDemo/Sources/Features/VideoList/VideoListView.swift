5fc5f096034b47fe979b69b5f45c0cbabaf20270
0Hooni <thddudgns972@gmail.com>
2025-11-02 17:42:50 +0900

    chore: 프로젝트 폴더 구조 생성

---
+++ VideoPlayerDemo/Sources/Features/VideoList/VideoListView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoListView.swift
+//  VideoPlayerDemo
+//
+//  Created by 송영훈 on 11/2/25.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoListView()
+}
---
+++ VideoPlayerDemo/Sources/Features/VideoPlayer/VideoPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoPlayerView.swift
+//  VideoPlayerDemo
+//
+//  Created by 송영훈 on 11/2/25.
+//
+
+import SwiftUI
+
+struct VideoPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoPlayerView()
+}
