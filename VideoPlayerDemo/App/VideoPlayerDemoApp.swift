fbe611d7ab30199a2e2e3f31112c121c3494dce1
YeongHun Song <thddudgns972@gmail.com>
2025-11-02 18:08:56 +0900

    프로젝트 초기 설정 (#11)
    * remove: TCA 아키텍처 제거
    
    * chore: 프로젝트 폴더 구조 생성
    
    * chore: 백그라운드 모드 활성화
    
    * chore: 로컬 재생용 샘플 비디오 추가
    
    - 20초
    - 1분

---
+++ VideoPlayerDemo/App/VideoPlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerDemoApp.swift
+//  VideoPlayerDemo
+//
+//  Created by 송영훈 on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VideoListView()
+        }
+    }
+}
